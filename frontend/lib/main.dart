d6e40d43ae5be2f4ebeab95fbf3abe70b6019872
Nguyen Hieu <alexfoxy1424@gmail.com>
2024-09-21 14:24:21 +0700

    no command

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -10,9 +10,47 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+      debugShowCheckedModeBanner: false,
+      title: 'Ứng dụng full-stack flutter đơn giản',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final controller = TextEditingController();
+  String responseMessage = '';
+  void sendName() {
+    final name = controller.text;
+    controller.clear();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Ứng dụng full-stack flutter đơn giản')),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: controller,
+              decoration: InputDecoration(labelText: 'Tên'),
+            ),
+            SizedBox(height: 20),
+            FilledButton(onPressed: sendName, child: Text('Gửi')),
+            Text(
+              responseMessage,
+              style: Theme.of(context).textTheme.titleLarge,
+            )
+          ],
         ),
       ),
     );
