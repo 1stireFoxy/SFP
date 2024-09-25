01458f2fd423a8d8a54c8c08bc9275b086d6803c
1stireFoxy <alexfoxy1424@gmail.com>
2024-09-25 10:54:04 +0700

    no comment

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,5 +1,7 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 void main() {
   runApp(const MainApp());
 }
@@ -27,25 +29,51 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   final controller = TextEditingController();
   String responseMessage = '';
-  void sendName() {
+Future <void> sendName() async {
     final name = controller.text;
     controller.clear();
+    final url = Uri.parse('http://localhost:8080/api/v1/submit');
+    try {
+      final response = http.post(
+      url,
+      headers: {'Content-Type': 'application/json'},
+      body: json.encode({'name': name}),
+      )
+      .timeout(const Duration(seconds: 10));
+    if (response.body.IsNotEmpty){
+      final data = json.decode(response.body);
+      setState(() {
+        responseMessage = data['message'];
+      });
+     }
+    else {
+      setState(() {
+        responseMessage = 'không nhận được phản hồi từ server';
+      });
+     }
+    } 
+    catch (e) {
+      setState(() {
+        responseMessage = 'Đã xảy ra lỗi: ${e.toString()}';
+      });
+      
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Ứng dụng full-stack flutter đơn giản')),
+      appBar: AppBar(title: const Text('Ứng dụng full-stack flutter đơn giản')),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Column(
           children: [
             TextField(
               controller: controller,
-              decoration: InputDecoration(labelText: 'Tên'),
+              decoration: const InputDecoration(labelText: 'Tên'),
             ),
-            SizedBox(height: 20),
-            FilledButton(onPressed: sendName, child: Text('Gửi')),
+            const SizedBox(height: 20),
+            FilledButton(onPressed: sendName, child: const Text('Gửi')),
             Text(
               responseMessage,
               style: Theme.of(context).textTheme.titleLarge,
@@ -56,3 +84,9 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
+extension on Future<http.Response> {
+  get body => null;
+}
+
+
